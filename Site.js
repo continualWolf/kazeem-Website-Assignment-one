4ccc1c143295bcdba3ea8adc1811471ac750b72f
Ben Watt <77812385+continualWolf@users.noreply.github.com>
2022-01-24 16:02:29 +0000

    LightMode

---
+++ Site.js
@@ -0,0 +1,42 @@
+function OpenMenu(){
+	var test = document.getElementsByClassName("mobile-menu")[0];
+
+	if (test.style.opacity == "0") {
+test.style.width = "25em";
+test.style.opacity = "1";
+} else{
+		test.style.width = "0em";
+		test.style.opacity = "0";
+	}
+}
+function formSubmit(){
+	toastr.success('Success messages');
+}
+window.onload = function(){
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+}
+// Get the modal
+
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
